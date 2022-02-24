6f2fa00aeebebd48007046143382f8f462435abe
Kashif Firoz <firozkashif1234@gmail.com>
2022-02-25 00:40:06 +0530

    added localstorage functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Records from "./components/Records";
 import Transaction from "./components/Transaction";
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [income, setIncome] = useState(0);
-  const [expense, setExpense] = useState(0);
+  const [transactions, setTransactions] = useState(
+    () => JSON.parse(localStorage.getItem("transactions")) ?? []
+  );
+  const [amount, setAmount] = useState(
+    transactions.reduce((acc, current) => {
+      return acc + current.amount;
+    }, 0)
+  );
+  const [income, setIncome] = useState(
+    transactions.reduce((acc, current) => {
+      return current.amount > 0 ? acc + current.amount : acc;
+    }, 0)
+  );
+  const [expense, setExpense] = useState(
+    transactions.reduce((acc, current) => {
+      return current.amount < 0 ? acc + current.amount : acc;
+    }, 0)
+  );
 
   const handleDelete = (id) => {
     const transaction = transactions.find((t) => t.id === id);
@@ -21,6 +35,10 @@ function App() {
     setAmount((prev) => prev - transaction.amount);
     setTransactions((prev) => prev.filter((t) => t.id !== id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
 
   return (
     <>
